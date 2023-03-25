7152a7f9df4d1e7e2d8da58cc2261c132b07eb68
bruno magistrati <magistratibruno@gmail.com>
2023-03-25 16:25:27 -0300

    seo e imagenes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// $(document).ready(function(){
+	
 	// AGREGANDO CLASE ACTIVE AL PRIMER ENLACE ====================
 	$('.category_list .category_item[category="all"]').addClass('ct_item-active');
 
